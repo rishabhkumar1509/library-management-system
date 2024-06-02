16c1a5392817dc871af3a0aeaa18ccab5a0311d5
Rishabh Kumar Sajwal <rishabhkumar1509@gmail.com>
2024-06-02 19:47:39 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'mysql://rks:password0369@localhost:3306/librarymanagementdb'
+DATABASE_URL = 'mysql://user:pass@localhost:3306/librarymanagementdb'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
